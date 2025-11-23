a2a4ca134dade41d0b93b45ea3ecf08634b0e655
skakked <skakked@gmail.com>
2025-11-22 22:45:44 -0500

    Defined infinity as maximum integer value

--- dijkstra.c
+++ dijkstra.c
@@ -4,3 +4,10 @@
  * CS 5008
  * Dijkstra's algorithm implementation 
  */
+
+#include "dijkstra.h" // Include the header file
+#include <stdlib.h> 
+#include <limits.h>
+#include <stdbool.h>
+
+#define INFINITY_DIST INT_MAX // Define infinity as maximum integer value
