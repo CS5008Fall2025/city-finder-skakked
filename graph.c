3532560812690c5701a2b3f5ea485d02c3a50f60
skakked <skakked@gmail.com>
2025-11-22 22:05:00 -0500

    Added standard libraries and other cooments

--- graph.c
+++ graph.c
@@ -1,5 +1,12 @@
 /**
  * Name: Siddharth Kakked
  * Semester: Fall 2025
- * CS 5008
- * 
+ * CS 5008\
+ * Implementation of graph data structure using adjacency list representation
+ */
+ 
+#include "graph.h" // Include the corresponding header file
+#include <stdio.h> // For printf
+#include <stdlib.h> // For malloc, free
+#include <string.h> // For strcpy, strcmp
+
